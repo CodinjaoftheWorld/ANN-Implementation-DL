0bfe81604783a0f3f117f128c89a17fb0e6fd73b
Gaurav <gauravsaini.728@gmail.com>
2021-10-16 17:40:54 +0530

    training file updated

--- src/training.py
+++ src/training.py
@@ -1,5 +1,6 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
+from src.utils.model import create_model
 import argparse
 
 
@@ -7,10 +8,23 @@ import argparse
 
 def trainig(config_path):
     config = read_config(config_path)
-    
-    validation_datasize = config["params"]["validation_datasize"]
 
+    validation_datasize = config["params"]["validation_datasize"]
+    LOSS_FUNCTION = config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    NUM_CLASSES = config["params"]["num_classes"]
+    
     (X_train, y_train), (X_val, y_val), (X_test, y_test) = get_data(validation_datasize)
+    
+
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION_SET = (X_val, y_val)
+
+    history = model.fit(X_train, y_train, epochs=EPOCHS,
+                        validation_data=VALIDATION_SET)
 
 
 if __name__ == '__main__':
